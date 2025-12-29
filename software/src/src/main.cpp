3f987f99212a91ee4fda560858e3448e084b0954
Matthias Hess <mat.hess@bluewin.ch>
2025-12-29 08:59:57 +0100

    commit

--- software/src/src/main.cpp
+++ software/src/src/main.cpp
@@ -8,28 +8,30 @@
 #include "Encoder.h"
 #include <MaxFanState.h>
 #include <MaxFanDisplay.h>
-#include <ButtonArray.h>
-#include<MaxErrors.h>
+#include <MaxErrors.h>
+
+// HIER: Alte ButtonArray Lib raus, neue rein
+#include "ChordInput.h" 
 
 #define ENCODER_BUTTON 8
 #define MODE_BUTTON 10
 #define COVER_BUTTON 9
 
-
 MaxFanBLE fanBLE;
-long testValue =0;
+long testValue = 0;
 
 MaxFanState maxFanState;
 
 MaxRemote fanRemote(2);
 MaxReceiver fanIrReceiver(3);
-ButtonArray buttons({ENCODER_BUTTON,MODE_BUTTON,COVER_BUTTON});
+
+ChordInput buttons({ENCODER_BUTTON, MODE_BUTTON, COVER_BUTTON});
+
 MaxFanDisplay fanDisplay(6, 7);
 Encoder encoder(4, 5);
 
-
 void onBLECommand(const String& json) {
-  MaxError error=maxFanState.SetJson(json);
+  MaxError error = maxFanState.SetJson(json);
   if (error != MaxError::NONE)
     fanDisplay.showError(error);
 }
@@ -38,8 +40,8 @@ void setup() {
   Serial.begin(115200);
   delay(2000); 
 
-  // 1. Erst das Display initialisieren (hier wird Wire.begin() aufgerufen)
-  if (!fanDisplay.begin()) { // Adresse meist 0x3C
+  // 1. Erst das Display initialisieren
+  if (!fanDisplay.begin()) { 
     Serial.println(F("SSD1306 allocation failed"));
     for (;;); 
   }
@@ -47,7 +49,8 @@ void setup() {
   // 2. ERST JETZT die Geschwindigkeit erhöhen
   Wire.setClock(400000); 
   
-  buttons.begin();
+  // HIER: buttons.begin() entfällt, da Setup jetzt im Konstruktor passiert!
+  
   encoder.begin();
   encoder.reset();
   fanIrReceiver.begin();
@@ -57,6 +60,12 @@ void setup() {
 }
 
 void loop() {
+  static unsigned long lastButtonCheck = 0;
+  if (millis() - lastButtonCheck >= 20) {
+      buttons.tick(); 
+      lastButtonCheck = millis();
+  }
+
   static bool wasConnected = false;
   bool isConnected = fanBLE.isConnected();
   
@@ -66,12 +75,10 @@ void loop() {
   } else if (!isConnected && wasConnected) {
     Serial.println("BLE client disconnected");
     wasConnected = false;
-    // Restart advertising
     BLEDevice::startAdvertising();
   }
 
   fanRemote.send(maxFanState);
-
   fanDisplay.update(maxFanState, isConnected, testValue);
   
   if(fanBLE.isConnected()){
@@ -82,68 +89,70 @@ void loop() {
 
   int delta = encoder.getDelta();
   if(delta != 0){
-    switch (maxFanState.GetMode())
-    {
-      case MaxFanMode::OFF:
-        break;
-
+    switch (maxFanState.GetMode()) {
+      case MaxFanMode::OFF: break;
       case MaxFanMode::MANUAL:
         maxFanState.SetSpeed(maxFanState.GetSpeed() - 10*delta);
         break;
-
       case MaxFanMode::AUTO:
         maxFanState.SetTempCelsius(maxFanState.GetTempCelsius()-delta);
         break;
-        
-      default:
-        break;
+      default: break;
     }
   }
 
-  if(buttons.wasPressed(ENCODER_BUTTON)){
-    Serial.println("ENCODER_BUTTON");
-    maxFanState.SetAirFlow(maxFanState.GetAirFlow()==MaxFanDirection::IN ? MaxFanDirection::OUT: MaxFanDirection::IN);
-    
+    if (buttons.hasEvent()) {
+      
+      // Das Event aus der Queue holen
+      KeyEvent event = buttons.popEvent();
+
+      // --- Fall A: Encoder Button gedrückt ---
+      if (event.IsSingle(ENCODER_BUTTON)) {
+          Serial.println("EVENT: ENCODER_BUTTON");
+          maxFanState.SetAirFlow(maxFanState.GetAirFlow()==MaxFanDirection::IN ? MaxFanDirection::OUT: MaxFanDirection::IN);
+      }
+      
+      // --- Fall B: Cover Button gedrückt ---
+      else if (event.IsSingle(COVER_BUTTON)) {
+          Serial.println("EVENT: COVER_BUTTON");
+          // Debugging Output beibehalten
+          Serial.println(maxFanState.GetStateByte(), BIN);
+          maxFanState.SetCover(maxFanState.GetCover()==CoverState::CLOSED ? CoverState::OPEN: CoverState::CLOSED);
+          Serial.println(maxFanState.GetStateByte(), BIN);
+      }
+
+      // --- Fall C: Mode Button gedrückt ---
+      else if (event.IsSingle(MODE_BUTTON)) {
+          Serial.println("EVENT: MODE_BUTTON");
+          switch (maxFanState.GetMode()) {
+            case MaxFanMode::OFF:
+              Serial.println("OFF->MANUAL");
+              maxFanState.SetMode(MaxFanMode::MANUAL);
+              break;
+            case MaxFanMode::MANUAL:
+              Serial.println("MANUAL->AUTO");
+              maxFanState.SetMode(MaxFanMode::AUTO);
+              break;
+            case MaxFanMode::AUTO:
+              Serial.println("AUTO->OFF");
+              maxFanState.SetMode(MaxFanMode::OFF);
+              // Debug Check wie im Original
+              if(maxFanState.GetMode() == MaxFanMode::OFF){
+                 Serial.println("OFF as commanded");
+              } else {
+                 Serial.println("NOT OFF !!");
+              }
+              break;
+            default: break;
+          }
+      }
+
+      // --- Fall D: NEU! Kombinationen (Beispiel) ---
+      // Hier könntest du jetzt magische Kombinationen einbauen
+      else if (event.IsChord(MODE_BUTTON, COVER_BUTTON)) {
+          Serial.println("EVENT: MODE + COVER gleichzeitig!");
+          Serial.println("Secret Reset triggered...");
+          // maxFanState.Reset(); // oder ähnliches
+      }
   }
-  
-  if (buttons.wasPressed(COVER_BUTTON)){
-    Serial.println("COVER_BUTTON");
-    Serial.println(maxFanState.GetStateByte(),BIN);
-    maxFanState.SetCover(maxFanState.GetCover()==CoverState::CLOSED ? CoverState::OPEN: CoverState::CLOSED);
-    Serial.println(maxFanState.GetStateByte(),BIN);
-    
-  }
-    
-
-  if (buttons.wasPressed(MODE_BUTTON)){
-    Serial.println("MODE_BUTTON");
-    switch (maxFanState.GetMode())
-    {
-      case MaxFanMode::OFF:
-        Serial.println("OFF->MANUAL");
-        maxFanState.SetMode(MaxFanMode::MANUAL);
-        break;
-
-      case MaxFanMode::MANUAL:
-        Serial.println("MANUAL->AUTO");
-        maxFanState.SetMode(MaxFanMode::AUTO);
-        break;
-
-      case MaxFanMode::AUTO:
-        Serial.println("AUTO->OFF");
-        maxFanState.SetMode(MaxFanMode::OFF);
-        if( maxFanState.GetMode() == MaxFanMode::OFF){
-           Serial.println("OFF as commanded");
-        } else {
-          Serial.println("NOT OFF !!");
-        }
-        break;
-        
-      default:
-        break;
-    }
-    
-  }
-
-  
 }
